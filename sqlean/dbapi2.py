7d75fffb859b7390413bcc2f8b756bfb68cbca4f
Mark S. <the@wondersmith.dev>
2023-09-19 15:24:55 -0400

    style: docstrings + type annotations

--- sqlean/dbapi2.py
+++ sqlean/dbapi2.py
@@ -17,74 +17,92 @@
 # 2. Altered source versions must be plainly marked as such, and must not be
 #    misrepresented as being the original software.
 # 3. This notice may not be removed or altered from any source distribution.
+"""DB-API 2.0 interface for SQLite databases."""
 
-"""
-DB-API 2.0 interface for SQLite databases.
-"""
+# Future Imports
+from __future__ import annotations
 
+# Standard Library Imports
+import collections.abc
 import datetime
 import time
-import collections.abc
+from typing import Union
 
+# Third-Party Imports
 from sqlean._sqlite3 import *
 
-paramstyle = "qmark"
-
-threadsafety = 1
-
-apilevel = "2.0"
+apilevel: str = "2.0"
+paramstyle: str = "qmark"
+threadsafety: int = 1
 
 Date = datetime.date
-
 Time = datetime.time
-
+Binary = memoryview
 Timestamp = datetime.datetime
 
 
-def DateFromTicks(ticks):
+def DateFromTicks(ticks: Union[int, float]) -> Date:
+    """Convert the supplied `ticks` to the equivalent local date."""
     return Date(*time.localtime(ticks)[:3])
 
 
-def TimeFromTicks(ticks):
+def TimeFromTicks(ticks: Union[int, float]) -> Time:
+    """Convert the supplied `ticks` to the equivalent local time."""
     return Time(*time.localtime(ticks)[3:6])
 
 
-def TimestampFromTicks(ticks):
+def TimestampFromTicks(ticks: Union[int, float]) -> Timestamp:
+    """Convert the supplied `ticks` to the equivalent local datetime."""
     return Timestamp(*time.localtime(ticks)[:6])
 
 
-version_info = tuple([int(x) for x in version.split(".")])
-sqlite_version_info = tuple([int(x) for x in sqlite_version.split(".")])
-
-Binary = memoryview
-collections.abc.Sequence.register(Row)
+version_info = tuple(map(int, version.split(".")))
+sqlite_version_info = tuple(map(int, sqlite_version.split(".")))
+register_sequence_type = getattr(collections.abc.Sequence, "register", None)
 
 
-def register_adapters_and_converters():
-    def adapt_date(val):
-        return val.isoformat()
+if callable(register_sequence_type):
+    register_sequence_type(Row)
 
-    def adapt_datetime(val):
-        return val.isoformat(" ")
 
-    def convert_date(val):
-        return datetime.date(*map(int, val.split(b"-")))
+def register_adapters_and_converters() -> None:
+    """Register converter and adapter functions for the date/time types sqlite
+    doesn't handle natively."""
 
-    def convert_timestamp(val):
-        datepart, timepart = val.split(b" ")
-        year, month, day = map(int, datepart.split(b"-"))
-        timepart_full = timepart.split(b".")
-        hours, minutes, seconds = map(int, timepart_full[0].split(b":"))
-        if len(timepart_full) == 2:
-            microseconds = int("{:0<6.6}".format(timepart_full[1].decode()))
+    def adapt_date(value: datetime.date) -> str:
+        """Convert the supplied `value` to the equivalent iso-formatted
+        string."""
+        return value.isoformat()
+
+    def adapt_datetime(value: datetime.datetime) -> str:
+        """Convert the supplied `value` to the equivalent iso-formatted
+        string."""
+        return value.isoformat(" ")
+
+    def convert_date(value: bytes) -> Date:
+        """Convert the supplied raw `value` to a Python-native date object."""
+        return Date(*map(int, value.split(b"-")))
+
+    def convert_timestamp(value: bytes) -> Timestamp:
+        """Convert the supplied raw `value` to a Python-native datetime
+        object."""
+
+        date_part, time_part = value.split(b" ")
+        year, month, day = map(int, date_part.split(b"-"))
+        time_part_full = time_part.split(b".")
+        hours, minutes, seconds = map(int, time_part_full[0].split(b":"))
+
+        if len(time_part_full) == 2:
+            microseconds = int("{:0<6.6}".format(time_part_full[1].decode()))
         else:
             microseconds = 0
 
-        val = datetime.datetime(year, month, day, hours, minutes, seconds, microseconds)
-        return val
+        value = Timestamp(year, month, day, hours, minutes, seconds, microseconds)
 
-    register_adapter(datetime.date, adapt_date)
-    register_adapter(datetime.datetime, adapt_datetime)
+        return value
+
+    register_adapter(Date, adapt_date)
+    register_adapter(Timestamp, adapt_datetime)
     register_converter("date", convert_date)
     register_converter("timestamp", convert_timestamp)
 
@@ -92,5 +110,4 @@ def register_adapters_and_converters():
 register_adapters_and_converters()
 
 # Clean up namespace
-
-del register_adapters_and_converters
+del register_sequence_type, register_adapters_and_converters
